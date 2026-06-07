2072588334eae119e2b352630beafcedd8d61692
duha1dahamsheh <developer2@bytechjo.com>
2026-06-07 11:02:21 +0300

    update

---
+++ src/component/WorkBookPages/Unit10/WB_Unit10_Page63.jsx
@@ -0,0 +1,17 @@
+import React, { useRef } from "react";
+import page2 from "../../../assets/imgs/pages/WB_Right_3//Right Int WB G3 U10 Folder/Page 63.png";
+
+
+const WB_Unit10_Page63 = () => {
+  return (
+    <div
+      className="page1-img-wrapper"
+      style={{ backgroundImage: `url(${page2})` }}
+    >
+      
+   
+    </div>
+  );
+};
+
+export default WB_Unit10_Page63;
---
+++ src/component/WorkBookPages/Unit10/WB_Unit10_Page64.jsx
@@ -0,0 +1,17 @@
+import React, { useRef } from "react";
+import page2 from "../../../assets/imgs/pages/WB_Right_3//Right Int WB G3 U10 Folder/Page 64.png";
+
+
+const WB_Unit10_Page64 = () => {
+  return (
+    <div
+      className="page1-img-wrapper"
+      style={{ backgroundImage: `url(${page2})` }}
+    >
+      
+   
+    </div>
+  );
+};
+
+export default WB_Unit10_Page64;
